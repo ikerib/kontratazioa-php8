60e1497e1d76313fd95da4e607ee2b0a9d9354ae
ikerib <ikerib@gmail.com>
2022-02-19 15:48:12 +0100

    bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 cd /usr/src/app
-composer install
+
 php bin/console fos:js-routing:dump --format=json --target=public/js/fos_js_routes.json
 php bin/console ckeditor:install
 php bin/console assets:install --symlink public
